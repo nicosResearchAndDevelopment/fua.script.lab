3f6283b647aa25191b29903fc2b3e06d4ba2f1fb
jlangkau <jlangkau@nicos-rd.com>
2021-08-17 19:39:12 +0200

    work

--- lab.SRDF/test/test.lab.graph.js
+++ lab.SRDF/test/test.lab.graph.js
@@ -7,20 +7,20 @@ exports['graph'] = [
         //'foaf:familyName': {"@type": "xsd:string", "@value": "Langkau"},
         //'age':             {'@type': "xsd:nonNegativeInteger", "@value": "58"}
     }
-    //, {
-    //    '@id':             "https://www.nicos-rd.com/spetrac",
-    //    '@type':           "foaf:Person",
-    //    'foaf:givenName':  "Simon",
-    //    'foaf:familyName': "Petrac"
-    //}
-    //,
-    //{
-    //    '@id':        "https://www.nicos-rd.com/adminGroup",
-    //    '@type':      "foaf:Group",
-    //    'rdfs:label': {'@type': "xsd:string", '@value': "Admin Group", '@language': "en"},
-    //    'hasMember':  [
-    //        "https://www.nicos-rd.com/jlangkau",
-    //        "https://www.nicos-rd.com/spetrac"
-    //    ]
-    //}
+    , {
+        '@id':             "https://www.nicos-rd.com/spetrac",
+        '@type':           "foaf:Person",
+        'foaf:givenName':  "Simon",
+        'foaf:familyName': "Petrac"
+    }
+    ,
+    {
+        '@id':        "https://www.nicos-rd.com/adminGroup",
+        '@type':      "foaf:Group",
+        'rdfs:label': {'@type': "xsd:string", '@value': "Admin Group", '@language': "en"},
+        'hasMember':  [
+            "https://www.nicos-rd.com/jlangkau",
+            "https://www.nicos-rd.com/spetrac"
+        ]
+    }
 ];
